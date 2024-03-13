aeb9f0ff1264ce4e2e0044e2ead6e3df5f0057d5
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-03-13 15:05:36 -0400

    commit for check

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Game Logic
+
 function gameBoard() {
     const rows = 3;
     const columns = 3;
@@ -87,3 +89,61 @@ function checkForWinner(gameBoard, row, column) {
 
     return isRowWin || isColumnWin || leftDiagonalWin || rightDiagonalWin;
 }
+
+// DOM
+
+function gameStart() {
+    const p1NameInputBox = document.querySelector("#player-one-name");
+    const p2NameInputBox = document.querySelector("#player-two-name");
+
+    const p1SaveNameBtn = document.querySelector("#player-one-save-name");
+    const p2SaveNameBtn = document.querySelector("#player-two-save-name");
+
+    const p1Sign = document.querySelector("#xSign");
+    const p2Sign = document.querySelector("#oSign");
+
+    let p1Name = "";
+    let p2Name = "";
+
+    let playersAreReady = false;
+
+    const savePlayerName = (nameInputBox, sign, saveBtn) => {
+        nameInputBox.disabled = true;
+        nameInputBox.style.backgroundColor = "#fdba74";
+        nameInputBox.style.color = "black";
+        sign.style.opacity = "100%";
+        saveBtn.style.visibility = "hidden";
+        return nameInputBox.value;
+    };
+
+    p1SaveNameBtn.addEventListener("click", () => {
+        p1Name = savePlayerName(p1NameInputBox, p1Sign, p1SaveNameBtn);
+        playersAreReady = !p1Name === "" && !p2Name === "";
+    });
+
+    p2SaveNameBtn.addEventListener("click", () => {
+        p2Name = savePlayerName(p2NameInputBox, p2Sign, p2SaveNameBtn);
+        playersAreReady = !p1Name === "" && !p2Name === "";
+    });
+
+    return { p1Name, p2Name, playersAreReady };
+}
+
+function game() {
+    const start = gameStart();
+    console.log(start);
+    // const controller = gameController(start.p1Name, start.p2Name);
+
+    // const boardElement = document.querySelector(".board");
+    // const reportElement = document.querySelector(".game-report");
+
+    // // if (start.playersAreReady) {
+    // //     console.log(start.playersAreReady);
+    // //     boardElement.style.visibility = "visible";
+    // //     reportElement.style.visibility = "visible";
+    // // }
+
+    // const cellElement = document.querySelector(".spot");
+}
+
+game();
